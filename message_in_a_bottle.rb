0abbf19b6b84684c9b508eb4c6eea997facc8e61
Indigo253931 <indigo253931@gmail.com>
2016-07-16 12:55:43 -0600

    counter

--- message_in_a_bottle.rb
+++ message_in_a_bottle.rb
@@ -1,23 +1,21 @@
 class MessageInABottle < Sinatra::Base
-
 	@@counter = 1
 
 	get '/' do
 		erb :index
 	end
 
-
-	
 	get '/beach' do
-	@location = params['location']	
-		if @@counter == 5 
-			@@counter = 0
+		if @@counter % 5 == 0
+			@message = "Ouch! There was a crab in the bottle."
+		else
+			messages = {
+				'west' => "It's totally awesome to write messages in your used bottles at the end of a spring break rager!", 
+				'east' => 'Argh, the Island of Tortuga runs short of turtles.'
+			} 
+			@message = messages[params['location']]
 		end
-
 		@@counter += 1
-		@counter = @counter
-		puts @counter
 		erb :new
-	end
-		
+	end 
 end
